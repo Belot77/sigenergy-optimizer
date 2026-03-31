36d0ebdf9684d8e4fdedfd6bc101042d51ba02e1
Belot77 <Belot77@users.noreply.github.com>
2026-03-31 16:27:09 +1100

    Set base href from window.location at runtime, bump to haos9

--- app/routers/ui.py
+++ app/routers/ui.py
@@ -8,9 +8,4 @@ templates = Jinja2Templates(directory="templates")
 
 @ui.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    ingress_path = request.headers.get("X-Ingress-Path", "").rstrip("/")
-    return templates.TemplateResponse(
-        request=request,
-        name="index.html",
-        context={"ingress_path": ingress_path},
-    )
+    return templates.TemplateResponse(request=request, name="index.html")
